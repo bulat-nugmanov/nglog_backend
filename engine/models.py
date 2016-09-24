aa5386be19654f93b4b3c3f603f3d6b3041faff6
Bulat Nugmanov <bbnugmanov@ualberta.ca>
2016-09-24 13:17:09 -0700

    data model v0.1

--- engine/models.py
+++ engine/models.py
@@ -1,47 +1,49 @@
 from __future__ import unicode_literals
 from mongoengine import *
 
+# http://docs.mongoengine.org/guide/defining-documents.html#dealing-with-deletion-of-referred-documents
+
+ADDRESS_LEN = 100
+NAME_LEN = 25
+COMMENT_LEN = 200
+
 
 class Unit(Document):
-    serial = StringField(max_length=25)
-    name = StringField(max_length=50)
-    # logbook
-    # site
-    # contacts
-    # schematics (optional)
-    # photos (optional)
+    serial = StringField(max_length=25, unique=True, required=True)
+    name = StringField(max_length=NAME_LEN)
+    logs = ListField(ReferenceField(Log))
+    site = ReferenceField(Site, required=True)
+    contacts = ListField(ReferenceField(Contact))
+    # TODO schematics (optional)
+    # TODO photos (optional)
+    # TODO add indexing and QuerySet ordering for efficiency:
+    # meta = {}
 
 
 class Site(Document):
-    # name
-    # address
-    # lat lon
-    # units
-    # overview???
-    """"""
-
-
-class Logbook(Document):
-    """"""
-    # unit
-    # logs
+    name = StringField(max_length=25, unique=True, required=True)
+    address = StringField(max_length=ADDRESS_LEN)
+    units = ListField(ReferenceField(Unit))
+    # TODO loc = GeoPointField()
 
 
 class Log(Document):
-    """"""
+    user = ReferenceField('User')
+    site = ReferenceField(Site)
+    comments = ListField(ReferenceField(Comment))
+    checkin = DateTimeField(required=True)
+    checkout = DateTimeField(required=True)  # TODO figure out timing
 
 
 class Comment(Document):
-    text = StringField(max_length=150)
-    # log
-    # check in?
-    # user
+    text = StringField(max_length=COMMENT_LEN)
+    user = ReferenceField('User')  # TODO
 
 
 class Contact(Document):
-    """"""
-    # name
-    # number
-    # email
-    #
+    name = StringField(max_length=NAME_LEN, required=True)
+    organization = StringField(max_length=NAME_LEN)
+    address = StringField(max_length=ADDRESS_LEN)
+    number = StringField(max_length=15, required=True)
+    email = EmailField()
 
